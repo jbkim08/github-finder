fc2b1dcc7f8a634e0afa8275d4c8d47a9a994c2c
JB <drv98@naver.com>
2023-11-30 14:59:22 +0900

    SET_LOADING 로직 추가

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
@@ -6,10 +7,14 @@ const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true); //시작시 깃허브에서 데이터를 가져옴(작업중 true)
+  const initialState = {
+    users: [],
+    loading: false,
+  };
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading(); //로딩상태 true
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -17,12 +22,25 @@ export const GithubProvider = ({ children }) => {
     });
     const data = await response.json();
 
-    setUsers(data);
-    setLoading(false); //데이터 로딩 완료
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    });
   };
 
+  const setLoading = () =>
+    dispatch({
+      type: 'SET_LOADING',
+    });
+
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
