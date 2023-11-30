4077d86e5ecdc8c1b98991012c6a66ec50c6736b
JB <drv98@naver.com>
2023-11-30 14:24:31 +0900

    깃허브컨텍스트 적용

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,27 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true); //시작시 깃허브에서 데이터를 가져옴(작업중 true)
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []); //앱 시작시 실행됨
-
-  const fetchUsers = async () => {
-    //console.log(`${import.meta.env.VITE_GITHUB_URL}/users`);
-    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false); //데이터 로딩 완료
-  };
 
   if (!loading) {
     return (
---
+++ src/context/github/GithubContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+const GithubContext = createContext();
+
+const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); //시작시 깃허브에서 데이터를 가져옴(작업중 true)
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+
+    setUsers(data);
+    setLoading(false); //데이터 로딩 완료
+  };
+
+  return (
+    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
