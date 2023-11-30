a6a112c918e73f60dba02569b8319dcf472dad96
JB <drv98@naver.com>
2023-11-30 09:27:32 +0900

    초기화

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-xl">헬로우 월드!</h1>
+      <button className="btn">클릭!</button>
+    </div>
+  );
+}
+
+export default App;
