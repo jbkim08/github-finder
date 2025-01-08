9ded964d629d94b80dbea22c0315959a87f48ffb
JB <drv98@naver.com>
2025-01-08 09:39:17 +0900

    파일정리 및 테마적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">헬로우 월드!</h1>
-      <button className="btn">클릭!</button>
+    <div>
+      <h1>헬로우 월드!</h1>
     </div>
   );
 }
