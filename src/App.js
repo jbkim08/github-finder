bb89dad83e5388530d11de784acc5376686332b9
JB <drv98@naver.com>
2025-01-08 10:36:11 +0900

    푸터

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="flex flex-col justify-between h-screen">
       <Navbar title="Github Finder" />
-
       <main>Content</main>
+      <Footer />
     </div>
   );
 }
