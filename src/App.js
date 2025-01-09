68dbd1ba395fd212ea720ef826c187b3487a45c1
JB <drv98@naver.com>
2025-01-09 09:35:12 +0900

    프롭스 경고

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import Alert from "./components/layout/Alert";
+import { useState } from "react";
 
 function App() {
+  const [alert, setAlert] = useState("");
+  const handleAlert = (msg) => {
+    setAlert(msg);
+    //3초 뒤에 메세지 제거
+    setTimeout(() => {
+      setAlert("");
+    }, 3000);
+  };
   return (
     <GithubProvider>
       <BrowserRouter>
         <div className="flex flex-col justify-between h-screen">
           <Navbar title="Github Finder" />
           <main className="container mx-auto px-3 pb-12">
+            <Alert alert={alert} />
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home handleAlert={handleAlert} />} />
               <Route path="/about" element={<About />} />
               <Route path="/notfound" element={<NotFound />} />
               <Route path="*" element={<NotFound />} />
